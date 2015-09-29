9ec12db2bff20a51cbb1b446cdaece6ec78a1ec8
unknown <kellarso@mines.edu>
2015-09-29 11:46:05 -0600

    Changed mouth size and placement.

--- DrawPlay/src/Cat.java
+++ DrawPlay/src/Cat.java
@@ -14,11 +14,11 @@ public class Cat {
 	private static final int EYE_HEIGHT = 20;
 	private static final int EYE_WIDTH = 15;
 	// pick mouth height, width is based on head dimension
-	private static final int MOUTH_HEIGHT = 5;
-	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
+	private static final int MOUTH_HEIGHT = 8;
+	private static final int MOUTH_WIDTH = HEAD_DIMENSION/2;
 	// mouth starts about 40% from left edge of head
-	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
-	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 2;
+	private static final int MOUTH_X = HEAD_DIMENSION/5 *1;
+	private static final int MOUTH_Y = HEAD_DIMENSION/5 *3;
 	
 	// draw will render the Cat on the Graphics object
 	public void draw(Graphics g, int catX, int catY)
